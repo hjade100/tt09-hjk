4568f29e22de07ca0693ce75f2472cb80876aa5e
Your Name <your_email@example.com>
2024-11-10 05:59:49 -0800

    Fix error 3

--- src/lif.v
+++ src/lif.v
@@ -1,11 +1,11 @@
-`default_nettype 
+`default_nettype none
  
  module lif (
     input wire [7:0]    current,
     input wire          clk,
     input wire          reset_n,
     output reg [7:0]    state,
-    output wire         spike,
+    output wire         spike
     );
 
 wire [7:0] next_state;
